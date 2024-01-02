823f27e687c63963904a23e7a7575e6897e4dcb0
Okumura0903 <info@muworks.net>
2024-01-02 18:18:51 +0900

    debugbar,docker,database

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    Indexaaaaa
+  </div>
+  <Link href="/hello">Show Page</Link>
+  <div>{{ message }}</div>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3'
+defineProps({
+  message: String,
+})
+</script>
+
+<!-- <script>
+import MainLayout from '../../Layouts/MainLayout.vue'
+export default {
+  layout: MainLayout
+}
+</script> -->
